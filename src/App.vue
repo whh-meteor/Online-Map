3a84705d7fdc13d95f188d245dda1cbc830eaccf
whh-meteor <654671523@qq.com>
2024-12-23 11:06:56 +0800

    ci:add cesium

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <OnlineMap msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import OnlineMap from "./components/OnlineMap.vue";
+
+export default {
+  name: "App",
+  components: {
+    OnlineMap,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/OnlineMap.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script>
+/* eslint-disable */
+import "../../public/Cesium/Widgets/widgets.css";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      viewer: null,
+    };
+  },
+  methods: {
+    initCesium() {
+      // Create a new Viewer object
+      this.viewer = new Cesium.Viewer("cesiumContainer");
+    },
+  },
+  mounted() {
+    this.initCesium();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
